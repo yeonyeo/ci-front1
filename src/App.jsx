661d457ea7e993b9321a8bf0eafc8f722039dd6b
lsy <sylim0513@gmail.com>
2024-05-29 10:18:20 +0900

    first

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [name, setName] = useState(0);
+  const [text, setText] = useState(0);
+  const [data, setData] = useState();
+  const getData = async () => {
+    try {
+      const response = await axios.get("http://34.125.107.233:8080/api/boards");
+      setData(response.data);
+      console.log(response);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const postData = async () => {
+    try {
+      const response = await axios.post(
+        "http://34.125.107.233:8080/api/boards",
+        {
+          name: name,
+          text: text,
+        }
+      );
+      console.log(response);
+      getData();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div>
+      <input type="text" onChange={(e) => setName(e.target.value)}></input>
+      <input type="text" onChange={(e) => setText(e.target.value)}></input>
+      <button onClick={postData}>submit</button>
+      {data?.map((element) => (
+        <div>
+          name:{element.name} title:{element.text}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
